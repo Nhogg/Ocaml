656390d922f820bfaa61f117a4e0f2be6d2f88a7
nhogg <nathanhogg1223@gmail.com>
2025-10-31 02:04:56 +0000

    Coursework

--- Coursework/coursework-nh-main/src/question2.ml
+++ Coursework/coursework-nh-main/src/question2.ml
@@ -3,6 +3,23 @@
 (** a. (3 marks) **)
 
 (* isValidPath : int -> int -> bool list -> bool *)
+(**
+This function takes the dimensions (m and n) of a grid and a boolean
+list representing the path (p). It defines an auxiliary function to "walk"
+the grid based on the path contained within p.
+To do this, we first match on the contents of remainingPath.
+If it is empty, we check if our coordinates are currently at the top right corner.
+If they are, the path reaches the correct point and we return true. Otherwise,
+our path was not correct and we return false.
+Next, if there are items in remainingPath, we know there are more steps left
+to take. If this si the case, we determine if it is true or false.
+If it is true, we calculate if going up by one would push us outside of the grid.
+If it does, then the path is not valid. Otherwise, we add 1 to the y coordinate
+and call walk again with the new coordinates and the rest of the list.
+This process repeats until we either go out of bounds or arrive at the top right
+of the grid.
+Example: isValidPath 2 2 [true; false; true; false] will return true
+**)
 let isValidPath m n p =
         let rec walk (currentX, currentY) remainingPath =
                 match remainingPath with
@@ -29,6 +46,22 @@ let isValidPath m n p =
 (** b. (3 marks) **)
 
 (* history : bool list -> (int * int) list *)
+(**
+This function returns the coordinates passed through by a path.
+It takes a boolean list and returns an (int * int) list. To do this,
+we first define an auxiliary function, buildHistory, that takes the pair
+(currentX, currentY), and a boolean list representing the remaining path
+that has not been analyzed. We first match on the remaining path.
+If the list is empty, we return the currentX and currentY as that is 
+the only available coordinates we have. Otherwise, we define (nextX, nextY)
+as the next coordinates reached by the current step in our path.
+After that, we recursively call buildHistory on (nextX, nextY) and the
+rest of the path to generate hte history of future coorinates. Finally,
+we prepend the current position to the result of that recursive call,
+which allows us to build the complete list of coordinates.
+Example: history [true; false; true] will return
+[(0,0); (0,1); (1,1); (1,2)]
+**)
 let history path = 
         let rec buildHistory (currentX, currentY) remainingPath =
                 match remainingPath with
@@ -53,6 +86,17 @@ let history path =
 (** c. (3 marks) **)
 
 (* turningNumber : bool list -> int *)
+(**
+This function determines the number of turns taken by a given path.
+It takes a path and returns an integer.
+To do this, we first match on the path. If the list is empty, there are
+zero turns. If the list only has one move, then that path also contains zero
+turns. Otherwise, we disect the list and determine how many unique transitions
+occur within the list. For example, the list [true; false; true; false] contains
+3 unique transitions. We calculate this by checking if h1 != h2. If so, then there
+has been a unique transition. Otherwise, there was no turn. We then
+add that result to the the result of the recursive call on the rest of the list.
+**)
 let rec turningNumber path =
         match path with
         | [] -> 0
@@ -64,42 +108,39 @@ let rec turningNumber path =
 (** d. (3 marks) **)
 
 (* allPaths : int -> int -> bool list list *)
-let rec allPaths m n = 
-        let rec prependMove move paths =
-               match paths with
-               | [] -> []
-               | h :: t -> (move :: h) :: (prependMove move t)
-        in
+(* Helper function to prepend a given move to every path in a list of paths. *)
+let rec prependMove move paths =
+        match paths with
+        | [] -> []
+        | h :: t -> (move :: h) :: (prependMove move t)
+
+(**
+This function computes all possible paths in an m-n grid.
+It takes the dimensions (m and n) and returns a list of boolean
+lists, where each boolean list represents a unique path from (0, 0) to
+(m, n). To do this, we first define prependMove that adds a given move to
+the front of every path in the list.
+Next, we pattern match on the coordinates. If both are 0, the only path
+is the empty list [[]]. Otherwise, we recursively compute paths that arrive
+from above when m > 0 and those that arrive from the left when n > 0.
+We then prepend true to the paths from above and false to the ones from the left.
+Finally, we concatenate both results and return the complete list of
+all possible paths.
+**)        
+let rec allPaths m n =
         match (m, n) with
         | (0, 0) -> [ [] ]
         | (m, n) ->
                         let upPaths =
                                 if m > 0 then
-                                        let pathsFromAbove = allPaths (m - 1) n in
-                                        prependMove true pathsFromAbove
+                                        prependMove true (allPaths (m - 1) n)
                                 else
                                         []
                         in
                         let rightPaths =
                                 if n > 0 then
-                                        let pathsFromLeft = allPaths m (n - 1) in
-                                        prependMove false pathsFromLeft
+                                        prependMove false (allPaths m (n - 1))
                                 else
                                         []
                         in
                         upPaths @ rightPaths
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- Coursework/coursework-nh-main/src/question3.ml
+++ Coursework/coursework-nh-main/src/question3.ml
@@ -3,27 +3,168 @@
 (** a. (3 marks) **)
 
 (* allDistinct : 'a list -> bool *)
-let allDistinct = failwith "Not implemented"
+(**
+This function determines if a given list's elements
+are all distinct. It takes a list and returns true or false.
+To do this we first define an auxiliary function contains that
+takes an element and a list. We pattern match on that list. If the list
+if empty, we return false as there are no elements. Otherwise, we
+check if the head of the list is equal to that the element. If it is,
+then the list contains that element. Otherwise, we move forward and check
+the next element. Outside the aux function, we do a simiar check
+and see if the list contains two of the same elements. If it does, then
+we return false. Otherwise, we continue checking other elements.
+**)
+let rec allDistinct list =
+        let rec contains element list =
+                match list with
+                | [] -> false
+                | h :: t -> if h = element then true else contains element t
+        in
+        match list with
+        | [] -> true
+        | h :: t ->
+                        if contains h t then
+                               false
+                        else allDistinct t 
 
 (** b. (2 marks) **)
 
 (* allEqual : 'a list -> bool *)
-let allEqual = failwith "Not implemented"
+(**
+This function determines if all the elements in a list
+are equal. It takes a list and returns either true or false.
+We first match on the list. If it is empty, then we return true
+as there are no elements and thus they are all equal. If it only
+contains one element, we return true. Otherwise, we disect the
+list and check if the first and second element are equal.
+If they are, we recursively check the rest of the list. If they are not,
+then we return false since there are two different elements in the list.
+**)
+let rec allEqual list =
+        match list with
+        | [] -> true
+        | [x] -> true
+        | h1 :: h2 :: t ->
+                        if h1 = h2 then
+                                allEqual (h2 :: t)
+                        else false
+
 
 (** c. (3 marks) **)
 
 (* groupConsecutive : 'a list -> 'a list list *)
-let groupConsecutive = failwith "Not implemented"
+(**
+This function takes a list and returns a list of lists
+that contain all consecutive, equal elements grouped together.
+To do this, we match on the list. If it is empty, we simply
+return an empty list as there are no elements. If it contains
+one element, we return a list containing that element.
+Otherwise, we compare the first two elements. If they are equal,
+we recursively group the the tail and merge the current element with
+the head of the first group in the recusrive result.
+If they are different, we start a new group with the current element and
+continue grouping the rest of the list.
+**)
+let rec groupConsecutive list =
+        match list with
+        | [] -> []
+        | [x] -> [[x]]
+        | h1 :: h2 :: t ->
+                        if h1 = h2 then
+                                match groupConsecutive (h2 :: t) with
+                                | [] -> [[h1]]
+                                | (x :: xs) -> (h1 :: x) :: xs
+                        else
+                                [h1] :: groupConsecutive (h2 :: t)
 
 (** d. (3 marks) **)
 
-let groupPred1 = failwith "Not implemented"
-let groupPred2 = failwith "Not implemented"
+(* Grouping a list then flattening it returns the original sequence. *)
+(* This function takes a list of lists and concatenates them into a single list *)
+let rec flatten list =
+        let rec append a b =
+                match a with
+                | [] -> b
+                | h :: t -> h :: append t b
+        in
+        match list with
+        | [] -> []
+        | h :: t -> append h (flatten t)
 
-let groupPred1Test = failwith "Not implemented"
-let groupPred2Test = failwith "Not implemented"
+(**
+This predicate shows the property that grouping and flattening a list should
+reproduce the original list. groupConsecutive cannot lose elements,
+reorder elements, or create duplicates.
+**)
+let groupPred1 list =
+        flatten (groupConsecutive list) = list
+
+(* All lists returned by groupConsecutive are non empty and contain equal values. *)
+
+(**
+This function checks that every sublist in a list of lists is valid.
+Each group must be non-empty and all elements within a group must be
+equal.
+**)
+let rec allGroupsValid groups =
+        match groups with
+        | [] -> true
+        | h :: t ->
+                        (h <> [] && allEqual h) && allGroupsValid t
+
+(**
+This predicate shows the property that all groups created by groupConsecutive are
+valid, meaning that no empty groups are created and all values in each
+group are the same.
+**)
+let groupPred2 list =
+        allGroupsValid (groupConsecutive list)
+
+open QCheck
+
+let groupPred1Test =
+        Test.make
+        ~name:"groupConsecutive keeps original list order and elements"
+        (list int)
+        (fun list -> groupPred1 list)
+
+let groupPred2Test =
+        Test.make
+        ~name:"groupConsecutive groups are non-empty and contain equal values"
+        (list int)
+        (fun list -> groupPred2 list)
 
 (** e. (2 marks) **)
 
 (* allEqualWithWitness : 'a list -> 'a option * bool *)
-let allEqualWithWitness = failwith "Not implemented"
+(**
+This function generalizes allEqlau by returning a boolean indicating
+whether all elements in the list are equal and an optional "witness" value
+that represents the repeated value.
+We use foldLeft to traverse the list while maintaining key state 
+information: acc_ok (boolean tracking whether all elements seen so far are
+equal) and acc_val (the reference value that subsequent values are compared to.)
+We first match the list. If it is empty, we return (None, true) as an empty list
+satisfies this equality. Otherwise, we take the head value as the initial reference value
+and fold to traverse the rest of the list. We compare each element (x) to acc_val.
+If there is a mismatch, acc_ok becomes false and stays false. Finally, if ok is true, it
+returns (Some value, true) otherwise (None, false.)
+**)
+let rec foldLeft v f xs =
+        match xs with
+        | [] -> v
+        | x :: xs -> foldLeft (f v x) f xs
+
+let allEqualWithWitness list =
+        match list with
+        | [] -> (None, true)
+        | h :: t ->
+                        let (ok, value) =
+                                foldLeft (true, h)
+                                (fun (acc_ok, acc_val) x ->
+                                        (acc_ok && (x = acc_val), acc_val))
+                                t
+                        in
+                        if ok then (Some value, true) else (None, false)
+
